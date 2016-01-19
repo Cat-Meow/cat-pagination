e50bbd6f0699d6a6d439e2ad5afefe8b9d0e6bd7
yanweiqing <ywq1991@gmail.com>
2016-01-20 00:11:32 +0800

    微重构pagination

--- examples/simple-style.jsx
+++ examples/simple-style.jsx
@@ -4,47 +4,46 @@ import Pagination from '../src/pagination.js';
 import '../assets/index.less';
 
 class Example extends Component {
-    constructor(props) {
-        super(props);
-    }
 
-    state = {
-        offset: 0
-    }
+  state = {
+    offset: 0
+  }
 
-    _updatePage = (offset) => {
-        console.log(`收到 ${offset}`);
-        this.setState({
-            offset: offset
-        });
-    }
+  _updatePage(offset) {
+    console.log(`收到 ${offset}`);
+    this.setState({
+      offset: offset
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <Pagination
-                    offset={this.state.offset}
-                    update={this._updatePage}
-                />
+  render() {
+    let { offset } = this.state;
+    return (
+      <div>
+        <Pagination offset={ offset } update={ this._updatePage.bind(this) }/>
 
-                <hr />
-                <Pagination
-                    offset={this.state.offset}
-                    totalPage={16}
-                    update={this._updatePage}
-                    myStyle="compressed"
-                />
+        <hr />
 
-                <hr />
-                <Pagination
-                    offset={this.state.offset}
-                    totalPage={16}
-                    update={this._updatePage}
-                    myStyle="fullsize"
-                />
-            </div>
-        );
-    }
+        <Pagination offset={ offset } update={ this._updatePage.bind(this) }
+          totalPage={16}
+          myStyle="compressed"
+        />
+
+        <hr />
+
+        <Pagination offset={this.state.offset} update={this._updatePage.bind(this)}
+          totalPage={16}
+        />
+
+        <hr />
+
+        <Pagination offset={this.state.offset} update={this._updatePage.bind(this)}
+          totalPage={16}
+          myStyle="fullsize"
+        />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<Example />, document.getElementById('component-example-simple-style'));
--- src/pager.js
+++ src/pager.js
@@ -1,52 +1,56 @@
 import React, { Component } from 'react';
+import { setClass } from 'cat-util';
 
 class Pager extends Component {
-    static propTypes = {
-        page: React.PropTypes.string,
-        target: React.PropTypes.number,
-        totalPage: React.PropTypes.number
-    }
+  static propTypes = {
+    content: React.PropTypes.string, // 显示内容
+    offset: React.PropTypes.number, // 当前显示的offset
+    target: React.PropTypes.number, // 点击的目标跳转
+    totalPage: React.PropTypes.number // 总页码
+  }
 
-    static defaultProps = {
-        page: '',
-        target: -1,
-        totalPage: 0
-    }
+  static defaultProps = {
+    content: '',
+    offset: 0,
+    target: -1,
+    totalPage: 0
+  }
 
-    state = {
-        disabled: this.props.target < 0 || (this.props.totalPage !== -1 && this.props.target >= this.props.totalPage)
-    }
+  state = {
+    disabled: this._getDis(this.props)
+  }
 
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            disabled: nextProps.target < 0 || (nextProps.totalPage !== -1 && nextProps.target >= nextProps.totalPage)
-        });
-    }
-    
-    constructor(props) {
-        super(props);
-    }
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      disabled: this._getDis(nextProps)
+    });
+  }
 
-    _handleClick(event) {
-        event.preventDefault();
-        if (!this.state.disabled) {
-            this.props.handleClick(this.props.target);
-        }
-    }
+  _getDis(props) {
+    return props.target < 0 || (props.totalPage !== -1 && props.target >= props.totalPage);
+  }
 
-    render() {
-        let disabled = this.state.disabled ? 'disabled' : '',
-            active = this.props.target === this.props.offset ? ' active' : '',
-            classes = `${disabled} ${active}`;
-
-        return (
-            <li className={classes}>
-                <a href="#" onClick={this._handleClick.bind(this)}>
-                    {this.props.page}
-                </a>
-            </li>
-        );
+  _handleClick(event) {
+    event.preventDefault();
+    if (!this.state.disabled) {
+      this.props.handleClick(this.props.target);
     }
+  }
+
+  render() {
+    let classes = setClass({
+      disabled: this.state.disabled,
+      active: this.props.target === this.props.offset
+    });
+
+    return (
+      <li className={classes}>
+        <a href="#" onClick={this._handleClick.bind(this)}>
+          {this.props.content}
+        </a>
+      </li>
+    );
+  }
 }
 
 export default Pager;
